9341dd49ad7eaa0a8827d657f4b225a60e502019
Krazy0 <57904740+krazyyyy@users.noreply.github.com>
2021-11-03 18:38:25 +0500

    Update urls.py

--- newsPortal/urls.py
+++ newsPortal/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('rss/', include('rssReader.urls'))
+    path('rss/', include('rssReader.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
